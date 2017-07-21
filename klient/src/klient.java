be21ede1e31b77c8377d405256e08a9f3ae5100e
AwangardowyKaloryfer <lopl233@o2.pl>
2017-07-22 00:17:14 +0200

    klient wersja pół-gotowa

--- klient/src/klient.java
+++ klient/src/klient.java
@@ -2,7 +2,7 @@
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.util.Scanner;
 import java.io.*;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -15,7 +15,7 @@ class Klient_reciver {
     private  PrintWriter pw;
     private  BufferedReader br;
 
-    Klient_reciver(){
+    Klient_reciver() throws IOException {
 
         String currentDir = System.getProperty("user.dir")+"/testkeysore.p12";
         System.setProperty("javax.net.ssl.keyStore",currentDir);
@@ -29,19 +29,27 @@ class Klient_reciver {
         try {
             sslConnector.sslsocket.startHandshake();
         } catch (IOException e) {}
+        pw = new PrintWriter(sslConnector.sslsocket.getOutputStream());
+        br = new BufferedReader(new InputStreamReader(sslConnector.sslsocket.getInputStream()));
 
     }
 
     public JSONObject reciver(Map map) throws IOException, JSONException {
         JSONObject message = new JSONObject(map);
 
+        System.out.println("----Sending to server----");
+        System.out.println(JsonToString(message));
         pw.write(message.toString());
         pw.write("\n");
         pw.flush();
 
         String serverAnswer = br.readLine();
+        JSONObject answer = new JSONObject(serverAnswer);
 
-        return new JSONObject(serverAnswer);
+        System.out.println("----Answer from server");
+        System.out.println(JsonToString(answer));
+
+        return answer;
     }
 
     public  JSONObject logIn(String Login, String Password, int device_id) throws JSONException, IOException {
@@ -98,13 +106,40 @@ class Klient_reciver {
 class main{
     public static Klient_reciver klient_reciver;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, JSONException {
         klient_reciver = new Klient_reciver();
+        Scanner scanner = new Scanner(System.in);
+        String choice = "";
         while(true){
+            System.out.println("1. Login");
+            System.out.println("2. Get user data");
+            System.out.println("3. Add device");
+            System.out.println("4. Login");
+            System.out.println("5. Login");
 
 
+            choice = scanner.next();
+            switch (choice) {
+                case "1":
+                    System.out.println("Login : ");
+                    String login = scanner.next();
+                    System.out.println("Haslo : ");
+                    String password = scanner.next();
+                    System.out.println("ID urzadzenia : ");
+                    int device_id = scanner.nextInt();
+                    klient_reciver.logIn(login, password, device_id);
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
 
 
+            }
 
 
         }
